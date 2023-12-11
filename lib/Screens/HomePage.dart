012fbb2257d3c4404f0d9c027d3167ed995a5829
NeilBanerjee2002 <neilbanerjee.22.5.2002@gmail.com>
2023-12-11 14:12:13 +0530

    initial homepage button not working

---
+++ lib/Screens/HomePage.dart
@@ -0,0 +1,58 @@
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:leafshield/utilities/utilities.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Uint8List? _image;
+
+
+  void selectImage()async{
+    Uint8List im = await pickImage(ImageSource.gallery);
+    setState(() {
+      _image = im;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Welcome to Leafshield !!',
+            style: kTextStyle,),
+            SizedBox(height: 20.0,),
+            InkWell(
+              onTap: ()async{
+                selectImage;
+              },
+              child: Container(
+                child: Center(child: Text('UPLOAD IMAGE'),),
+                width: 180.0,
+                height: 40.0,
+                decoration: const ShapeDecoration(shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
+                  color: Colors.orangeAccent,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utilities/utilities.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+const kTextStyle = TextStyle(
+  fontFamily: 'Sofia',
+  fontSize: 30.0,
+  fontWeight: FontWeight.w200,
+);
+
+Future pickImage(ImageSource source) async {
+  final ImagePicker _imagePicker = ImagePicker();
+
+  XFile? _file = await _imagePicker.pickImage(source: source);
+  if (_file != null) {
+    return await _file.readAsBytes();
+  }
+  print("no image selected");
+}
